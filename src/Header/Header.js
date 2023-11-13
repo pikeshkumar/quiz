b374a92fdf0d7ace49cc087b307af6cfa179a3f5
pikeshkumar <kumarpikesh52@gmail.com>
2023-11-13 12:29:10 +0530

    fixed header spacing

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,20 +4,20 @@ import "../Header/Header.css";
 import Logo from "../images/logo.png";
 
 function Header() {
-	return (
-		<div className="align header">
-			<NavLink className="logo-width">
-				<NavLink to="/">
-					<img src={Logo} alt="logo" width={"120px"} />
-				</NavLink>
-			</NavLink>
-			<div className="align menu-width">
-				<NavLink to="/">Home</NavLink>
-				{/* <NavLink to="/play-quiz">Play Quiz</NavLink> */}
-				<NavLink to="/my-quiz">My Quizes</NavLink>
-			</div>
-		</div>
-	);
+  return (
+    <div className="align header">
+      <NavLink className="logo-width">
+        <NavLink to="/">
+          <img src={Logo} alt="logo" width={"120px"} />
+        </NavLink>
+      </NavLink>
+      <div className="align actionLinks">
+        <NavLink to="/">Home</NavLink>
+        {/* <NavLink to="/play-quiz">Play Quiz</NavLink> */}
+        <NavLink to="/my-quiz">My Quizes</NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
